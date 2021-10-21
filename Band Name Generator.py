189aabab086eebc86293dae12eb5064b7111d06e
RJanell <92831974+RJanell@users.noreply.github.com>
2021-10-21 10:43:52 -0400

    Add files via upload

---
+++ Band Name Generator.py
@@ -0,0 +1,4 @@
+print('Welcome to the Band Name Genertator!')
+city= input('What city did you grow up in?')
+season= input('What is your favorite season?')
+print('The name of your band could be:', city + season)
